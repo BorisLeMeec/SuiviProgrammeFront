e1d9caf85ee41eb27c2e0848b4ad9b47955689ca
Pierre Capon <pierre.capon22@gmail.com>
2020-03-12 18:21:45 +0100

    add some style

--- src/ElectedList/Components/faceIcon.js
+++ src/ElectedList/Components/faceIcon.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import './faceIcon.scss'
+import React from "react";
+import "./faceIcon.scss";
 
 export default class faceIcon extends React.Component {
-	
-	constructor(props) {
-		super(props);
-		this.state = {}
-	}
-	render() {
-		return (
-			<div className="faceIcon" style={{backgroundImage: `url(${this.props.photoUrl})`}}>
-                {this.props.name}
-			</div>
-		)
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div
+        className="faceIcon"
+        style={{ backgroundImage: `url(${this.props.photoUrl})` }}
+      >
+        <div>
+          <span>{this.props.name}</span>
+        </div>
+      </div>
+    );
+  }
 }
--- src/ElectedList/Views/ElectedList.js
+++ src/ElectedList/Views/ElectedList.js
@@ -1,50 +1,69 @@
-import React from 'react';
-import FaceIcon from '../Components/faceIcon';
-import {
-	Link
-} from "react-router-dom";
+import React from "react";
+import FaceIcon from "../Components/faceIcon";
+import { Link } from "react-router-dom";
+import "./ElectedList.scss";
 
 // First we create our class
 export default class ElectedList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      elected: [
+        {
+          name: "Nathalie Appéré",
+          id: 1,
+          promessNbr: "5",
+          photoUrl:
+            "https://www.rennes-infos-autrement.fr/wp-content/uploads/Nathalie_App%C3%A9r%C3%A9_-_F%C3%A9vrier_2013_-_01-810x1215.jpg"
+        },
+        {
+          name: "Carole Gandon",
+          id: 2,
+          promessNbr: "4",
+          photoUrl:
+            "https://www.rennes-infos-autrement.fr/wp-content/uploads/th-1.jpeg"
+        },
+        {
+          name: "Matthieu Theurier",
+          id: 3,
+          promessNbr: "3",
+          photoUrl:
+            "https://pbs.twimg.com/profile_images/1200133121640976384/rCUvHm8__400x400.jpg"
+        },
+        {
+          name: "Enora Le Pape",
+          id: 4,
+          promessNbr: "7",
+          photoUrl:
+            "https://img.20mn.fr/TEDIw1xITKmMpnkYDWC6Fg/640x410_enora-le-pape-tete-de-liste-rennes-en-commun-la-france-insoumise-aux-elections-municipales-2020-a.jpg"
+        }
+      ]
+    };
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			elected: [
-				{ name: 'Nathalie Appéré', id: 1, promessNbr: '5', photoUrl: 'https://www.rennes-infos-autrement.fr/wp-content/uploads/Nathalie_App%C3%A9r%C3%A9_-_F%C3%A9vrier_2013_-_01-810x1215.jpg' },
-				{ name: 'Carole Gandon', id: 2, promessNbr: '4', photoUrl: 'https://www.rennes-infos-autrement.fr/wp-content/uploads/th-1.jpeg' },
-				{ name: 'Matthieu Theurier', id: 3, promessNbr: '3', photoUrl: 'https://pbs.twimg.com/profile_images/1200133121640976384/rCUvHm8__400x400.jpg' },
-				{ name: 'Enora Le Pape', id: 4, promessNbr: '7', photoUrl: 'https://img.20mn.fr/TEDIw1xITKmMpnkYDWC6Fg/640x410_enora-le-pape-tete-de-liste-rennes-en-commun-la-france-insoumise-aux-elections-municipales-2020-a.jpg' }
-			]
+  NumberList(elected) {
+    const listItems = elected.map(item => (
+      <div>
+        <Link to={`elected/${item.id}`}>
+          <FaceIcon key={item.id} name={item.name} photoUrl={item.photoUrl} />
+        </Link>
+      </div>
+    ));
+    return listItems;
+  }
 
-		}
-	}
-
-	NumberList(elected) {
-		const listItems = elected.map((item) =>
-			<Link to={`elected/${item.id}`}>
-				<FaceIcon
-					key={item.id}
-					name={item.name}
-					photoUrl={item.photoUrl}
-				/>
-			</Link>
-		);
-		return (
-			<ul>{listItems}</ul>
-		);
-	}
-
-	render() {
-		return (
-			<div>
-				<section className="ElectedList">
-					<p>{this.state.greeting} {this.state.name}</p>
-				</section>
-				<div>
-					{this.NumberList(this.state.elected)}
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <section className="ElectedList">
+          <p>
+            {this.state.greeting} {this.state.name}
+          </p>
+        </section>
+        <div className="elected-grid">
+          {this.NumberList(this.state.elected)}
+        </div>
+      </div>
+    );
+  }
 }
